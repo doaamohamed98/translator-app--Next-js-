4e6b213e43f46a1396669b93ea3e78cbe9a025d8
doaamohamed98 <mohammaddoaa64@gmail.com>
2024-08-03 19:04:49 +0300

    button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,15 @@
 import { NextPage } from 'next'
-import { Button } from '@mui/material/Button';
-
-interface Props {}
-
-const Button: NextPage<Props> = ({}) => {
-  return <div></div>
+import { Button } from '@mui/material';
+interface Props {
+    text:string
 }
 
-export default Button
+const Buttoon: NextPage<Props> = ({text}) => {
+  return <div>
+    <Button variant="contained" >
+          {text}
+    </Button>
+  </div>
+}
+
+export default Buttoon
