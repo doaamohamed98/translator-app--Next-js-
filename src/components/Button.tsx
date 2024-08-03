0c3ba6a398a3e50266be69e0c35d3fa803a53924
doaamohamed98 <mohammaddoaa64@gmail.com>
2024-08-03 19:03:28 +0300

    button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from 'next'
+import { Button } from '@mui/material/Button';
+
+interface Props {}
+
+const Button: NextPage<Props> = ({}) => {
+  return <div></div>
+}
+
+export default Button
