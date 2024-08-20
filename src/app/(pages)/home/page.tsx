d7657ed994cdb0d8f6c510cad7b558afaec38d51
doaamohamed98 <mohammaddoaa64@gmail.com>
2024-08-20 21:12:47 +0300

    create button logout to remove token

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -1,10 +1,26 @@
+"use client";
+import { Button, Typography } from '@mui/material'
 import { NextPage } from 'next'
+import Cookies from 'js-cookie'
+import { useRouter } from 'next/navigation';
 
 interface Props {}
 
 const Page: NextPage<Props> = ({}) => {
+  const router = useRouter();
+  const handleLogout = () => {
+    Cookies.remove('authToken')
+    router.push('/sign-in')
+
+  }
+
+
   return <div>
-    Home
+<Typography variant='h3' > Welcome To Home Page</Typography>
+
+<Button variant='contained' onClick={handleLogout} >
+  LogOut
+</Button>
   </div>
 }
 
